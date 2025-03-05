119032e427e63f24420ee895d059cab285f6c1a7
Lesterpaintstheworld <reynolds.nicorr@gmail.com>
2025-03-05 01:04:30 +0100

    deleted

--- components/Layout.js
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-black via-black to-dark-gray">
-      <main className="container mx-auto px-4 py-8">
-        {children}
-      </main>
-    </div>
-  );
-}
